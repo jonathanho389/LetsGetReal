0b3d4aa515131feda8e9cc28f9d5490db4af7431
Jonathan Ho <hojon389@gmail.com>
2020-11-23 21:08:00 -0500

    coded equals and add

--- RealNumber.java
+++ RealNumber.java
@@ -12,6 +12,20 @@ public class RealNumber{
   public String toString(){
     return "" + getValue();
   }
-
-  
+  public boolean equals(RealNumber other){
+    double diff1 = Math.abs(value - other.getValue())/value;
+    if(other.getValue() == 0 && value == 0){
+      return true;
+    }
+    else if(diff1 <= 0.001){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  public RealNumber add(RealNumber other){
+    RealNumber num = new RealNumber(value * other.getValue());
+    return num;
+  }
 }
