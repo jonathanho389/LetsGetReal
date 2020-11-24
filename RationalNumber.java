3ee2d3d2573d353e8b134d0f17557476737a76e1
Jonathan <hojon389@gmail.com>
2020-11-23 21:27:24 -0500

    coded rationalnumber constructor, getvalue, getnumerator, and getdenominator

--- RationalNumber.java
+++ RationalNumber.java
@@ -5,10 +5,21 @@ public class RationalNumber extends RealNumber{
     super(0.0);
     numerator = nume;
     denominator = deno;
+    if(denominator == 0){
+      numerator = 0;
+      denominator = numerator;
+    }
   }
 
   public double getValue(){
-    return numerator/denominator;
+    return (double)numerator/(double)denominator;
   }
 
+  public int getNumerator(){
+    return numerator;
+  }
+
+  public int getDenominator(){
+    return denominator;
+  }
 }
